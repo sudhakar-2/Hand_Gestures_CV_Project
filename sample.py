bee5b827a789f563aa34b8f6b22cbed4c60216b4
sudhakar-2 <indlasudhakar13@gmail.com>
2025-12-30 16:31:48 +0530

    changed by developer c

--- sample.py
+++ sample.py
@@ -2,4 +2,4 @@ import numpy as np
 
 print(np.__version__)
 print("this is just I am checking. not do anything in this file. You dont want to use it ")
-print("this is done by developer C")
+print("this is done by developer C. Nothing is changed")
